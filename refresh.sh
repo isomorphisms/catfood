c03f6f9bbdec7bc3316cc7e4a3dedfe31d5e0ac1
i <isomorphisms@sdf.org>
2026-08-24 17:36:07 -0400

    Keep Catfood updates on the UTF-8 root path

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eu
 
+catfood_locale=${CATFOOD_LOCALE:-C.UTF-8}
+LANG=$catfood_locale
+LC_ALL=$catfood_locale
+export LANG LC_ALL
+
 root=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
 workspace=${CATFOOD_ROOT:-/opt}
 
